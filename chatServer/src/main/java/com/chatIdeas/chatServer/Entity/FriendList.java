054b06dece67b76e61febc2fc15d9c432b48b5e2
Prasanna S <prasannas.22cse@kongu.edu>
2025-02-09 22:28:25 +0530

    Update FriendList.java

--- chatServer/src/main/java/com/chatIdeas/chatServer/Entity/FriendList.java
+++ chatServer/src/main/java/com/chatIdeas/chatServer/Entity/FriendList.java
@@ -7,7 +7,7 @@ import jakarta.persistence.*;
 public class FriendList {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "user_id") // The current user
