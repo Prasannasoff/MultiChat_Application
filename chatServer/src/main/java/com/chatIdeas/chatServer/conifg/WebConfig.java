a1b07d5fc7ee78ba2cec27642e022b87892c1670
Prasanna S <prasannas.22cse@kongu.edu>
2025-02-10 08:04:42 +0530

    Update WebConfig.java

--- chatServer/src/main/java/com/chatIdeas/chatServer/conifg/WebConfig.java
+++ chatServer/src/main/java/com/chatIdeas/chatServer/conifg/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
          registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://multichat-application-1.onrender.com") // Corrected syntax
+                .allowedOrigins("http://localhost:3000", "https://multi-chat-application.vercel.app") // Corrected syntax
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(false);
