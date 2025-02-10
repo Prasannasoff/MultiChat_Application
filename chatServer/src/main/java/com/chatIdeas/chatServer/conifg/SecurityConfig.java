5d9952950e4cfaeb91decf55007cec817e780b4e
Prasanna S <prasannas.22cse@kongu.edu>
2025-02-10 08:14:11 +0530

    Update SecurityConfig.java

--- chatServer/src/main/java/com/chatIdeas/chatServer/conifg/SecurityConfig.java
+++ chatServer/src/main/java/com/chatIdeas/chatServer/conifg/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.setAllowedOrigins(List.of("http://localhost:3000","https://multi-chat-application.vercel.app")); // Allow all origins
+       config.setAllowedOriginPatterns(List.of("http://localhost:3000", "https://multi-chat-application.vercel.app")); // Use patterns instead of fixed origins
         config.setAllowedHeaders(List.of("*")); // Allow all headers
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Allow all methods
 
